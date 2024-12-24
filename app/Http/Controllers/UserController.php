b41b513c51cb8fb9bf038b0a8e66675073fc3aa3
Lusan Maulana <lusan.maulana@gmail.com>
2024-12-24 11:54:04 +0700

    Add users API

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\user;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    /**
+     * Show users list.
+     */
+    public function show(FormRequest $request)
+    {
+        $search = $request->query('search', '');
+        $page = $request->query('page', 1);
+        $pageCount = $request->query('pageCount', 10);
+        $sortBy = $request->query('sortBy', 'created_at');
+        if (!in_array($sortBy, ['name', 'email', 'created_at'])) {
+            $sortBy = 'created_at';
+        }
+
+        $users = User::query()
+            ->where('active', true)
+            ->where('name', 'like', "%$search%")
+            ->orWhere('email', 'like', "%$search%")
+            ->skip(($page - 1) * $pageCount)
+            ->take($pageCount)
+            ->orderBy($sortBy)
+            ->get();
+
+        return response()->json([
+            'search' => $search,
+            'page' => $page,
+            'pageCount' => $pageCount,
+            'sortBy' => $sortBy,
+            'users' => $users,
+        ], 200);
+    }
+
+    /**
+     * Store a new user.
+     */
+    public function store(FormRequest $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|max:255',
+            'password' => 'required|string|min:8',
+            'name' => 'required|string|min:3|max:50',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation error',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        // $user = User::create($request->all());
+        $user = User::create($request->only(['email', 'password', 'name']));
+        return response()->json($user, 200);
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory;
+
+    public $timestamps = false;
+
+    /**
+    * The attributes that are mass assignable.
+    *
+    * @var array<int, string>
+    */
+   protected $fillable = [
+       'email',
+       'password',
+       'name',
+   ];
+
+   /**
+    * The attributes that should be hidden for serialization.
+    *
+    * @var array<int, string>
+    */
+   protected $hidden = [
+       'password',
+   ];
+
+   /**
+    * Get the attributes that should be cast.
+    *
+    * @return array<string, string>
+    */
+   protected function casts(): array
+   {
+       return [
+           'password' => 'hashed',
+       ];
+   }
+}
